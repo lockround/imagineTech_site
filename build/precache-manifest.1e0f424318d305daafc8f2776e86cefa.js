b0cad465bdf4a2f1298a6a0a853cac64ea8176ba
Shubham Tiwari <websbiz0000@gmail.com>
2019-01-17 10:30:56 +0000

    adds build

---
+++ build/precache-manifest.1e0f424318d305daafc8f2776e86cefa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e42099699402719f907",
+    "url": "/static/css/main.841e514d.chunk.css"
+  },
+  {
+    "revision": "2e42099699402719f907",
+    "url": "/static/js/main.2e420996.chunk.js"
+  },
+  {
+    "revision": "0229d5489b82668639d8",
+    "url": "/static/css/1.ab3e97e7.chunk.css"
+  },
+  {
+    "revision": "0229d5489b82668639d8",
+    "url": "/static/js/1.0229d548.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4e6b411326c988f2b6a642884291db1",
+    "url": "/index.html"
+  }
+];
